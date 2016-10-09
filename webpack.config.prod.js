3c3aa837962fb37046b8edfda0320fc95f046246
foisonocean <foisonocean@gmail.com>
2016-10-10 01:37:15 +0800

    optimizing webpack2 && add webpack production config

---
+++ webpack.config.prod.js
@@ -0,0 +1,42 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  devtool: false,
+  entry: [
+    'babel-polyfill',
+    './src/index'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/dist/'
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true,
+      debug: false
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: true
+      },
+      output: {
+        comments: false
+      },
+      sourceMap: false
+    })
+  ],
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      loaders: ['babel'],
+      include: path.join(__dirname, 'src')
+    }]
+  }
+};

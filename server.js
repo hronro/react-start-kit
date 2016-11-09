cb3db6ae5ff10c524c90b75a79d8843847bd317d
foisonocean <foisonocean@gmail.com>
2016-11-09 22:50:48 +0800

    use ES6 in server.js

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-var path = require('path');
-var webpack = require('webpack');
-var express = require('express');
-var devMiddleware = require('webpack-dev-middleware');
-var hotMiddleware = require('webpack-hot-middleware');
-var config = require('./webpack.config.dev');
+const path = require('path');
+const webpack = require('webpack');
+const express = require('express');
+const devMiddleware = require('webpack-dev-middleware');
+const hotMiddleware = require('webpack-hot-middleware');
+const config = require('./webpack.config.dev');
 
-var app = express();
-var compiler = webpack(config);
+const app = express();
+const compiler = webpack(config);
 
 app.use(devMiddleware(compiler, {
   publicPath: config.output.publicPath
@@ -14,12 +14,12 @@ app.use(devMiddleware(compiler, {
 
 app.use(hotMiddleware(compiler));
 
-app.get('*', function (req, res) {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 
-app.listen(3000, function(err) {
+app.listen(3000, err => {
   if(err) {
     return console.error(err);
   }

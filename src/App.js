3a4edc554552de428ace77f9aabea4114c6f956d
foisonocean <foisonocean@gmail.com>
2016-12-21 22:38:45 +0800

    use the new demo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 const App = () => (
   <div>
-    Demo
+    <h1>Demo</h1>
+    <Counter />
   </div>
 );
+
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
+    this.changeCount = this.changeCount.bind(this);
+  }
+
+  changeCount() {
+    this.setState({ count: this.state.count + 10 });
+  }
+
+  render() {
+    return (
+      <div>
+        { this.state.count }
+        <br />
+        <button onClick={this.changeCount}>click</button>
+      </div>
+    );
+  }
+
+}
 
 export default App;

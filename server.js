4837be304cda5b1def29d05749a27e448963a260
foisonocean <foisonocean@gmail.com>
2017-01-03 14:36:47 +0800

    use koa2

--- server.js
+++ server.js
@@ -1,28 +1,45 @@
-const path = require('path');
-const webpack = require('webpack');
-const express = require('express');
-const devMiddleware = require('webpack-dev-middleware');
-const hotMiddleware = require('webpack-hot-middleware');
-const config = require('./webpack.config.dev');
+import path from 'path';
+import fs from 'fs';
 
-const app = express();
-const compiler = webpack(config);
+import Koa from 'koa';
+import webpack from 'webpack';
+import { devMiddleware, hotMiddleware } from 'koa-webpack-middleware';
+
+import webpackConfig from './webpack.config.dev';
+
+const app = new Koa();
+const compiler = webpack(webpackConfig);
 
 app.use(devMiddleware(compiler, {
-  publicPath: config.output.publicPath
+  // display no info to console (only warnings and errors)
+  noInfo: true,
+
+  // display nothing to the console
+  quiet: true,
+
+  // public path to bind the middleware to
+  // use the same as in webpack
+  publicPath: webpackConfig.output.publicPath,
+
+  // options for formating the statistics
+  stats: {
+      colors: true,
+  },
 }));
 
 app.use(hotMiddleware(compiler));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+// send index.html
+app.use(ctx => {
+  ctx.type = 'text/html; charset=utf-8';
+  ctx.body = fs.readFileSync(path.join(__dirname, './index.html'));
+});
+
+app.on('error', function(err){
+  console.error('server error', err);
 });
 
 
-app.listen(3000, err => {
-  if(err) {
-    return console.error(err);
-  }
-
-  console.log('Listening at 🌏 http://localhost:3000/');
+app.listen(3000, () => {
+  console.log('Listening at 🌏  http://localhost:3000/');
 });

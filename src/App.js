77d6f836e289ee2839f12dee6e09e350cf381bee
foisonocean <foisonocean@gmail.com>
2017-02-08 15:53:57 +0800

    Lint the code

--- src/App.js
+++ src/App.js
@@ -1,35 +1,36 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
 const App = () => (
   <div>
     <h1>Demo</h1>
     <Counter />
   </div>
-);
+)
 
 class Counter extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      count: 0,
-    };
-    this.changeCount = this.changeCount.bind(this);
+  state = {
+    count: 0
   }
 
-  changeCount() {
-    this.setState({ count: this.state.count + 10 });
+  changeCount = () => {
+    this.setState({ count: this.state.count + 10 })
   }
 
-  render() {
+  handleClick = event => {
+    event.preventDefault()
+    this.changeCount()
+  }
+
+  render () {
     return (
       <div>
         { this.state.count }
         <br />
-        <button onClick={this.changeCount}>click</button>
+        <button onClick={this.handleClick}>click</button>
       </div>
-    );
+    )
   }
 
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,23 +1,23 @@
-import { AppContainer } from 'react-hot-loader';
-import React from 'react';
-import { render } from 'react-dom';
-import App from './App';
+import { AppContainer } from 'react-hot-loader'
+import React from 'react'
+import { render } from 'react-dom'
+import App from './App'
 
-const rootEl = document.getElementById('root');
+const rootEl = document.getElementById('root')
 render(
   <AppContainer>
     <App />
   </AppContainer>,
   rootEl
-);
+)
 
-if(module.hot) {
+if (module.hot) {
   module.hot.accept('./App', () => {
     render(
       <AppContainer>
         <App />
       </AppContainer>,
       rootEl
-    );
-  });
+    )
+  })
 }
